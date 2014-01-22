9c8190737f05aac73492bd2a702b865e0c3360e4
Bernard Labno <bernard.labno@itcrowd.pl>
2014-01-22 23:52:21 +0100

    ng-repeat

--- app/TodoCtrl.js
+++ app/TodoCtrl.js
@@ -1,6 +1,10 @@
 angular.module("TodoApp", []).controller("TodoCtrl", function ($scope)
 {
     $scope.user = {name: ""};
+    $scope.users = [
+        {name: "John", email: "john@example.com"},
+        {name: "Chris", email: "chris@example.com"}
+    ];
 
     $scope.greet = function (name)
     {
