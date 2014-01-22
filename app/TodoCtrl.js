c4df5ab58fb87f6b43d814cbaf5a2e770ea8428a
Bernard Labno <bernard.labno@itcrowd.pl>
2014-01-22 22:35:56 +0100

    More like a controller

--- app/TodoCtrl.js
+++ app/TodoCtrl.js
@@ -1,4 +1,10 @@
-function TodoCtrl($scope)
+angular.module("TodoApp", []).controller("TodoCtrl", function ($scope)
 {
     $scope.user = {name: ""};
-}
+
+    $scope.greet = function (name)
+    {
+        alert("Hello " + name);
+    };
+
+});
