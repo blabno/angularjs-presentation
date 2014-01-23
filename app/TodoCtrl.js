f6266603bbbeeb16bff589e89d25ed60438b16e0
Bernard Labno <bernard.labno@itcrowd.pl>
2014-01-23 09:44:26 +0100

    ngAnimate

--- app/TodoCtrl.js
+++ app/TodoCtrl.js
@@ -1,11 +1,15 @@
-var todoApp = angular.module("TodoApp", ["markdownModule"]).controller("TodoCtrl", function ($scope, UserDAO)
+var todoApp = angular.module("TodoApp", ["ngAnimate", "markdownModule"]).controller("TodoCtrl", function ($scope, UserDAO)
 {
     $scope.user = {name: ""};
     $scope.users = [];
 
     $scope.greet = function (name)
     {
-        UserDAO.save({name: name, email: name + "@example"}).success(refreshUsers);
+        var user = {name: name, email: name + "@example"};
+        UserDAO.save(user).success(function ()
+        {
+            $scope.users.push(user);
+        });
     };
 
     function refreshUsers()
